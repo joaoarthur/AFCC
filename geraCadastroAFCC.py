3cc317c91dba3bd7aa0d8c0e20ef8c494b7eb2d9
joaoarthur <joaoarthur.rj@gmail.com>
2019-12-17 08:53:16 -0300

    Update geraCadastroAFCC.py

--- geraCadastroAFCC.py
+++ geraCadastroAFCC.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+from unicodedata import normalize
 
 from viacep import ViaCEP
+
+def tell_me_about(s):
+    print(type(s), s)
+
+def remover_acentos(txt):
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
 def retorna_Endereco(cep):
     data = ViaCEP(cep)
     endereco = data.getDadosCEP()
-    if endereco == 'CEP NÃO INFORMADO':
+    if endereco == 'CEP NÃO INFORMADO' or endereco == 'CEP NÃO ENCONTRADO':
         return None, None, None, None
     else:
         return endereco['logradouro'], endereco['bairro'], endereco['localidade'], endereco['uf']
@@ -33,7 +40,7 @@ def retorna_CEP(visitante, constantes):
                               cidade=visitante[constantes['cidade']])
                 enderecos = data.getDadosEndereco()
                 if not enderecos:
-                    return "Endereco nao encontrado","NA","NA","NA"
+                    return "Endereco nao encontrado","","",""
                 else:
                     for endereco in enderecos:
                         if endereco['bairro'].upper() == visitante[constantes['bairro']].upper() or not visitante[constantes['bairro']]:
@@ -65,13 +72,13 @@ def retorna_CEP(visitante, constantes):
             elif enderecos != "ENDERECO NAO INFORMADO":
                 return enderecos[0]['cep'], enderecos[0]['bairro'], enderecos[0]['localidade'], enderecos[0]['uf']
             else:
-                return enderecos,"NA","NA","NA"
+                return "","","",""
         elif enderecos != "ENDERECO NAO INFORMADO":
             return enderecos[0]['cep'], enderecos[0]['bairro'], enderecos[0]['localidade'], enderecos[0]['uf']
         else:
-            return enderecos,"NA","NA","NA"
+            return "","","",""
     else:
-        return "ENDERECO NAO INFORMADO","NA","NA","NA"
+        return "","","",""
 
 #funcao para retirar abreviacao e colocar primeira letra maiuscula
 def trataNome(nome):
@@ -84,7 +91,6 @@ def trataNome(nome):
 
 #Abrindo arquivo com dados dos Visitantes
 fh = open(r'C:\Users\oi273045\Downloads\Cadastro Visitantes - Cadastro Visitantes.tsv','r',encoding = 'utf8')
-
 #Pegando o Header do Arquivo para identificar os campos
 campos = fh.readline().upper().split("\t")
 
@@ -92,7 +98,8 @@ campos = fh.readline().upper().split("\t")
 constantes = {
     'nome':campos.index('NOME_COMPLETO'),
     'cep':campos.index('CEP'),
-    'rua':campos.index('ENDERECO'),
+    'rua': campos.index('ENDERECO'),
+    'rua_manual': campos.index('RUA_MANUAL'),
     'numero':campos.index('NUMERO'),
     'complemento':campos.index('COMPLEMENTO'),
     'bairro':campos.index('BAIRRO'),
@@ -122,9 +129,9 @@ fhTratados = open(r'tratados.csv','w',encoding = 'utf8')
 fhTratarManual = open(r'TratamentoManual.csv','w',encoding = 'utf8')
 
 #inserindo Headers
-#fhOut.writelines(",".join(campos))
 #fhOut.writelines(campos[constantes['nome']]+";"+campos[constantes['data_nascimento']]+";"+campos[constantes['email']]+";"+campos[constantes['sexo']]+";"+campos[constantes['estado_civil']]+";"+campos[constantes['telefone']]+";"+campos[constantes['cep']]+";"+campos[constantes['rua']]+";"+campos[constantes['numero']]+";"+campos[constantes['complemento']]+";"+campos[constantes['bairro']]+";"+campos[constantes['cidade']]+";"+campos[constantes['uf']]+";"+campos[constantes['culto']]+";"+campos[constantes['como_conheceu']]+";"+campos[constantes['membro_de_igreja']]+";"+campos[constantes['qual']]+";"+campos[constantes['status']]+";"+campos[constantes['observacao']]+";"+campos[constantes['voluntario']]+";"+campos[constantes['data_atendimento']])
-fhOut.writelines('nome_completo'+";"+'email'+";"+'tipo_documento'+";"+'numero_documento'+";"+'sexo'+";"+'estado_civil'+";"+'data_nascimento'+";"+'apelido'+";"+'login_skype'+";"+'trabalho'+";"+'cargo'+";"+'pais'+";"+'estado'+";"+'cidade'+";"+'cep'+";"+'logradouro'+";"+'numero_logradouro'+";"+'complemento_logradouro'+";"+'bairro'+";"+'ddd_telefone_fixo'+";"+'numero_telefone_fixo'+";"+'ddd_telefone_celular'+";"+'numero_telefone_celular'+";"+'ddd_telefone_outro'+";"+'numero_telefone_outro'+";"+'tipo_vinculo'+";"+'status'+";"+'imagem_perfil'+";"+'id_sistema_legado\n')
+#fhOut.writelines('nome_completo'+";"+'email'+";"+'tipo_documento'+";"+'numero_documento'+";"+'sexo'+";"+'estado_civil'+";"+'data_nascimento'+";"+'apelido'+";"+'login_skype'+";"+'trabalho'+";"+'cargo'+";"+'pais'+";"+'estado'+";"+'cidade'+";"+'cep'+";"+'logradouro'+";"+'numero_logradouro'+";"+'complemento_logradouro'+";"+'bairro'+";"+'ddd_telefone_fixo'+";"+'numero_telefone_fixo'+";"+'ddd_telefone_celular'+";"+'numero_telefone_celular'+";"+'ddd_telefone_outro'+";"+'numero_telefone_outro'+";"+'tipo_vinculo'+";"+'status'+";"+'imagem_perfil'+";"+'id_sistema_legado\n')
+fhOut.writelines('nome_completo;email;tipo_documento;numero_documento;sexo;estado_civil;data_nascimento;apelido;login_skype;trabalho;cargo;pais;estado;cidade;cep;logradouro;numero_logradouro;complemento_logradouro;bairro;ddd_telefone_fixo;numero_telefone_fixo;ddd_telefone_celular;numero_telefone_celular;ddd_telefone_outro;numero_telefone_outro;tipo_vinculo;status;imagem_perfil;id_sistema_legado;Filhos;data_de_casamento;data_de_entrada;data_de_saida;facebook;grau_de_escolaridade;quantidade_de_filhos  ;nome_pai;nome_mae;nome_conjuge;consagracao;ultima_igreja_que_frequentou;data_de_batismo;voluntariado;data_filiacao;informacoes_extras_membro;nome_da_celula;batizado;curso_biblico;tamanho_blusa;confirmacao\n')
 fhTratarManual.writelines(";".join(campos))
 
 erro=False
@@ -132,6 +139,11 @@ erro=False
 #Para cada linha no arquivo de entrada
 for cadastro in fh:
     visitante = cadastro.split("\t")
+    if visitante[constantes['rua']] == 'CEP NAO ENCONTRADO':
+        visitante[constantes['rua']] = visitante[constantes['rua_manual']]
+        visitante[constantes['cep']] = ''
+    elif not visitante[constantes['rua']]:
+        visitante[constantes['rua']] = visitante[constantes['rua_manual']]
 
     if not visitante[constantes['cep']] or visitante[constantes['cep']] == "#ERROR!":
         #Se visitante nao possui CEP, Busca CEP pelo endereco
@@ -140,42 +152,81 @@ for cadastro in fh:
         if not visitante[constantes['cep']] or visitante[constantes['cep']] == "#ERROR!":
             erro = True
             fhTratarManual.writelines(";".join(visitante))
+            continue
         else:
-            visitante[constantes['rua']], visitante[constantes['bairro']], visitante[constantes['cidade']], visitante[constantes['uf']] = retorna_Endereco(visitante[constantes['cep']])
+            rua, bairro, cidade, uf = retorna_Endereco(visitante[constantes['cep']])
+            if rua == None:
+                erro = True
+                fhTratarManual.writelines(";".join(visitante))
+                continue
+            else:
+                visitante[constantes['rua']], visitante[constantes['bairro']], visitante[constantes['cidade']], visitante[constantes['uf']] = rua, bairro, cidade, uf
+
+
+    # remove acentuacao
+    visitante[constantes['rua']] = remover_acentos(visitante[constantes['rua']])
+    visitante[constantes['bairro']] = remover_acentos(visitante[constantes['bairro']])
+    visitante[constantes['cidade']] = remover_acentos(visitante[constantes['cidade']])
+    #visitante[constantes['rua']] = bytes(visitante[constantes['rua']], 'utf-8').decode('latin1')
+    #visitante[constantes['bairro']] = bytes(visitante[constantes['bairro']], 'utf-8').decode('latin1')
+    #visitante[constantes['cidade']] = bytes(visitante[constantes['cidade']], 'utf-8').decode('latin1')
+
     #Trata campo Nome para retirar abreviacao e Colocar primeira letra maiuscula e as outras minusculas.
     visitante[constantes['nome']]=" ".join(map(trataNome, visitante[constantes['nome']].split(" ")))
+    #remove acentuacao
+    visitante[constantes['nome']]=remover_acentos(visitante[constantes['nome']])
+    #visitante[constantes['nome']] = bytes(visitante[constantes['nome']], 'utf-8').decode('latin1')
+
     visitante[constantes['voluntario']]=" ".join(map(trataNome, visitante[constantes['voluntario']].split(" ")))
+    # remove acentuacao
+    #visitante[constantes['voluntario']] = bytes(visitante[constantes['voluntario']], 'utf-8').decode('latin1')
+    visitante[constantes['voluntario']]=remover_acentos(visitante[constantes['voluntario']])
+
+
     visitante[constantes['telefone']] = re.sub(r'(\D*)', r'', visitante[constantes['telefone']])
 
-    if (len(visitante[constantes['telefone']]) == 11 and visitante[constantes['telefone']][2] == '9') or (len(visitante[constantes['telefone']]) == 9 and visitante[constantes['telefone']][0] == '9'):
-        if(len(visitante[constantes['telefone']]) == 11):
-            telefone =';;'+visitante[constantes['telefone']][0:2]+';'+visitante[constantes['telefone']][2:]+';;'
+    if visitante[constantes['telefone']]:
+        if (len(visitante[constantes['telefone']]) == 11 and visitante[constantes['telefone']][2] == '9') or (len(visitante[constantes['telefone']]) == 9 and visitante[constantes['telefone']][0] == '9'):
+            if(len(visitante[constantes['telefone']]) == 11):
+                telefone =';;'+visitante[constantes['telefone']][0:2]+';'+visitante[constantes['telefone']][2:]+';;'
+            else:
+                telefone = ';;21;' + visitante[constantes['telefone']] + ';;'
+        elif (len(visitante[constantes['telefone']]) == 10 and visitante[constantes['telefone']][2] != '9') or (len(visitante[constantes['telefone']]) == 8 and visitante[constantes['telefone']][0] != '9'):
+            if (len(visitante[constantes['telefone']]) == 10):
+                telefone = visitante[constantes['telefone']][0:2]+';'+ visitante[constantes['telefone']][2:]+';;;;'
+            else:
+                telefone = '21;'+ visitante[constantes['telefone']] +';;;;'
         else:
-            telefone = ';;21;' + visitante[constantes['telefone']][2:] + ';;'
-    elif (len(visitante[constantes['telefone']]) == 10 and visitante[constantes['telefone']][2] != '9') or (len(visitante[constantes['telefone']]) == 8 and visitante[constantes['telefone']][0] != '9'):
-        if (len(visitante[constantes['telefone']]) == 10):
-            telefone = visitante[constantes['telefone']][0:2]+';'+ visitante[constantes['telefone']][2:]+';;;;'
-        else:
-            telefone = '21;'+ visitante[constantes['telefone']][2:] +';;;;'
+            erro = True
+            fhTratarManual.writelines(";".join(visitante))
+            continue
     else:
-        erro = True
-        fhTratarManual.writelines(";".join(visitante))
+        telefone = ';;;;;'
 
-    if visitante[constantes['estado_civil']][-1] == ")":
-        if visitante[constantes['sexo']] == 'F':
-            visitante[constantes['estado_civil']] = visitante[constantes['estado_civil']][0:-4]+"a"
-        else:
+    if visitante[constantes['estado_civil']]:
+        if visitante[constantes['estado_civil']][-1] == ")":
             visitante[constantes['estado_civil']] = visitante[constantes['estado_civil']][0:-3]
-
-    if visitante[constantes['cep']] == 'ENDERECO NAO INFORMADO':
+        else:
+            visitante[constantes['estado_civil']] = remover_acentos("_".join(visitante[constantes['estado_civil']].split(" ")).lower())
+    else:
+        visitante[constantes['estado_civil']]='nao_informado'
+    #    if visitante[constantes['sexo']] == 'F':
+    #        visitante[constantes['estado_civil']] = visitante[constantes['estado_civil']][0:-4]+"a"
+    #    else:
+    #        visitante[constantes['estado_civil']] = visitante[constantes['estado_civil']][0:-3]
+    visitante[constantes['qual']] = remover_acentos(visitante[constantes['qual']])
+    #visitante[constantes['estado_civil']] = bytes(visitante[constantes['estado_civil']], 'utf8').decode('latin1')
+    if visitante[constantes['cep']] == 'Endereco nao encontrado':
         #Se nao encontrou CEP, jogar para tratamento manual
         erro=True
         fhTratarManual.writelines(";".join(visitante))
     else:
         #fhOut.writelines(",".join(visitante))
         #fhOut.writelines(visitante[constantes['nome']]+";"+visitante[constantes['data_nascimento']]+";"+visitante[constantes['email']]+";"+visitante[constantes['sexo']]+";"+visitante[constantes['estado_civil']]+";"+visitante[constantes['telefone']]+";"+visitante[constantes['cep']]+";"+visitante[constantes['rua']]+";"+visitante[constantes['numero']]+";"+visitante[constantes['complemento']]+";"+visitante[constantes['bairro']]+";"+visitante[constantes['cidade']]+";"+visitante[constantes['uf']]+";"+visitante[constantes['culto']]+";"+visitante[constantes['como_conheceu']]+";"+visitante[constantes['membro_de_igreja']]+";"+visitante[constantes['qual']]+";"+visitante[constantes['status']]+";"+visitante[constantes['observacao']]+";"+visitante[constantes['voluntario']]+";"+visitante[constantes['data_atendimento']])
-        fhOut.writelines(visitante[constantes['nome']]+";"+visitante[constantes['email']].lower()+";;;"+visitante[constantes['sexo']].lower()+";"+visitante[constantes['estado_civil']].lower()+";"+visitante[constantes['data_nascimento']]+";;;;;BR;"+visitante[constantes['uf']]+";"+visitante[constantes['cidade']]+";"+visitante[constantes['cep']]+";"+visitante[constantes['rua']]+";"+visitante[constantes['numero']]+";"+visitante[constantes['complemento']]+";"+visitante[constantes['bairro']]+";"+telefone+";nao_membro;17;;;\n")
+        fhOut.writelines(visitante[constantes['nome']]+";"+visitante[constantes['email']].lower()+";;;"+visitante[constantes['sexo']].lower()+";"+visitante[constantes['estado_civil']].lower()+";"+visitante[constantes['data_nascimento']]+";;;;;BR;"+visitante[constantes['uf']]+";"+visitante[constantes['cidade']]+";"+visitante[constantes['cep']]+";"+visitante[constantes['rua']]+";"+visitante[constantes['numero']]+";"+visitante[constantes['complemento']]+";"+visitante[constantes['bairro']]+";"+telefone+";nao_membro;17;;;;;;;;;;;;;;"+visitante[constantes['qual']]+";;;"+visitante[constantes['data_atendimento']][0:-1]+";;;;;;\n")
         fhTratados.writelines(';'.join(visitante))
+
+
 #Fecha arquivos do processamento
 fh.close()
 fhOut.close()
